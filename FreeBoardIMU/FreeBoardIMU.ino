2cc5194a7c151fb435f792c303443db2029efe5a
joabakk <github@heiamoss.com>
2017-05-26 08:58:43 +0200

    Added Signal K output

--- FreeBoardIMU/FreeBoardIMU.ino
+++ FreeBoardIMU/FreeBoardIMU.ino
@@ -46,10 +46,11 @@ float val[9];
 float declination=0.0;
 bool raw_out=false;
 bool nmea_out=false;
-bool freeboard_out=true;
+bool freeboard_out=false;
+bool SK_out=true;
 String inputSerial = ""; // a string to hold incoming data
 
-typedef volatile float rval; //change float to the datatype you want to use
+typedef volatile double rval; //change float to the datatype you want to use
 const byte MAX_NUMBER_OF_READINGS = 5;
 rval mghStorage[MAX_NUMBER_OF_READINGS] = {0.0};
 AverageList<rval> mghList = AverageList<rval>( mghStorage, MAX_NUMBER_OF_READINGS );
@@ -79,11 +80,11 @@ void calculate() {
 void setup() {
 	inputSerial.reserve(40);
   Serial.begin(38400);
-  //Serial.begin(115200);
+  //Serial.begin(115200);//for serial monitor debugging
   Wire.begin();
-  
+
   my3IMU.init(true);
-  
+
   //setup timers
   FlexiTimer2::set(100, calculate); // 100ms period
   FlexiTimer2::start();
@@ -116,19 +117,20 @@ void serialEvent() {
 	}
 }
 void loop() {
+
   if(raw_out){
-         outputRaw(); 
+         outputRaw();
       }else if (execute) {
-       
+
         //do these every 100ms
         //quarternary, updates AHRS
         my3IMU.getQ(q);
-        
+
         if (interval % 2 == 0) {
           // do every 200ms
-            //mag 
+            //mag
             my3IMU.getValues(val);
-         
+
             //convert to YPR
             my3IMU.getYawPitchRollRad(yprm);
             //convert to magnetic heading
@@ -150,16 +152,20 @@ void loop() {
               printRateOfTurn();
               printPitchRoll();
            }
+           //And Signal K;
+           if(SK_out){
+            SK_output(h);
+           }
         }
    }
-        
+
         execute = false;
   }
-   
-    
- 
+
+
+
 void outputRaw(){
-  
+
         #if HAS_ITG3200()
           my3IMU.acc.readAccel(&raw_values[0], &raw_values[1], &raw_values[2]);
           //my3IMU.gyro.readGyroRaw(&raw_values[3], &raw_values[4], &raw_values[5]);
@@ -182,7 +188,7 @@ void outputRaw(){
         #endif
         Serial.println();
       //}
-  
+
 }
 
 void process(char * s, char parser) {
@@ -231,7 +237,7 @@ void process(char * s, char parser) {
                         //save calibration
                         if (strcmp(key, CAL_SAVE) == 0) {
                             const uint8_t eepromsize = sizeof(float) * 6 + sizeof(int) * 6;
-                            
+
                             while(Serial.available() < eepromsize){
                                 Serial.print(".") ; // wait until all calibration data are received
                             }
@@ -246,7 +252,7 @@ void process(char * s, char parser) {
                             digitalWrite(13, LOW);
                             Serial.println("Saved calibration to EEPROM");
                         }
-                        
+
 		} else {
 			strncpy(key, cmd, 3);
 			key[3] = '\0';
@@ -256,7 +262,7 @@ void process(char * s, char parser) {
 			//if (DEBUG) Serial.print(key);
 			//if (DEBUG) Serial.print(" = ");
 			//if (DEBUG) Serial.println(valArray);
-			
+
 			if (strcmp(key, DECL) == 0) {
 				declination= atof(valArray);
 			}
@@ -270,7 +276,7 @@ void process(char * s, char parser) {
                           Serial.print(",");
                           Serial.print(my3IMU.acc_off_z);
                           Serial.print("\n");
-                          
+
                           Serial.print("magn offset: ");
                           Serial.print(my3IMU.magn_off_x);
                           Serial.print(",");
@@ -278,7 +284,7 @@ void process(char * s, char parser) {
                           Serial.print(",");
                           Serial.print(my3IMU.magn_off_z);
                           Serial.print("\n");
-                          
+
                           Serial.print("acc scale: ");
                           Serial.print(my3IMU.acc_scale_x);
                           Serial.print(",");
@@ -286,7 +292,7 @@ void process(char * s, char parser) {
                           Serial.print(",");
                           Serial.print(my3IMU.acc_scale_z);
                           Serial.print("\n");
-                          
+
                           Serial.print("magn scale: ");
                           Serial.print(my3IMU.magn_scale_x);
                           Serial.print(",");
@@ -307,7 +313,7 @@ void printIMU(float h){
          if(freeboard_out){
             Serial.print("!!VER:1.9,UID:IMU,");
             Serial.print("MGH:");
-            
+
             Serial.print(h);
             Serial.print(",YAW:");
             Serial.print(degrees(yprm[0]));
@@ -316,8 +322,8 @@ void printIMU(float h){
             Serial.print(",RLL:");
             Serial.print(degrees(yprm[2]));
             Serial.println(",");
-         } 
-  
+         }
+
 }
 /*
 
@@ -334,9 +340,9 @@ Field Number:
 
     Checksum
    */
-   
+
 void printRateOfTurn(){
-  
+
    char rotSentence [25];
 
 	PString str(rotSentence, sizeof(rotSentence));
@@ -346,7 +352,7 @@ void printRateOfTurn(){
         dtostrf(r, 3, 1, rBuf);
 	str.print(rBuf); //prints 1 decimal, but round to degree, should be good enough
 	str.print(",A*");
-	
+
 	//calculate the checksum
 
 	int cs = 0; //clear any old checksum
@@ -357,9 +363,9 @@ void printRateOfTurn(){
 	if (cs < 0x10) str.print('0');
 	str.print(cs, HEX); // Assemble the final message and send it out the serial port
 	Serial.println(rotSentence);
-       
+
 }
-   
+
 /*
  Print out the NMEA string for mag heading
  $HC = mag compass
@@ -367,19 +373,19 @@ void printRateOfTurn(){
 
             Vessel heading in degrees with respect to magnetic north produced by
             any device or system producing magnetic heading.
-            
+
             ------------------------------------------------------------------------------
                     1   2 3
                     |   | |
              $--HDM,x.x,M*hh<CR><LF>
             ------------------------------------------------------------------------------
-            
-            Field Number: 
-            
+
+            Field Number:
+
             1. Heading Degrees, magnetic
             2. M = magnetic
             3. Checksum
-            
+
 */
 void printNmeaMag(float h){
     //Assemble a sentence of the various parts so that we can calculate the proper checksum
@@ -390,7 +396,7 @@ void printNmeaMag(float h){
 
 	str.print((int)h); //prints 1 decimal, but round to degree, should be good enough
 	str.print(".0,M*");
-	
+
 	//calculate the checksum
 
 	int cs = 0; //clear any old checksum
@@ -410,11 +416,11 @@ void printNmeaMag(float h){
                 float d = h-declination;
                 char dBuf[7];
                 dtostrf(d, 3, 1, dBuf);
-        	str.print(dBuf); 
+        	str.print(dBuf);
         	str.print(",T*");
-        	
+
         	//calculate the checksum
-        
+
         	int cs = 0; //clear any old checksum
         	for (unsigned int n = 1; n < strlen(hdtSentence) - 1; n++) {
         		cs ^= hdtSentence[n]; //calculates the checksum
@@ -422,8 +428,8 @@ void printNmeaMag(float h){
         	//bug - arduino prints 0x007 as 7, 0x02B as 2B, so we add it now
 		if (cs < 0x10) str.print('0');
         	str.print(cs, HEX); // Assemble the final message and send it out the serial port
-        	Serial.println(hdtSentence);  
-                
+        	Serial.println(hdtSentence);
+
                 //and do HDG (Heading, Deviation and Variation)
                 /*
                 ------------------------------------------------------------------------------
@@ -431,16 +437,16 @@ void printNmeaMag(float h){
                         |   |   | |   | |
                  $--HDG,x.x,x.x,a,x.x,a*hh<CR><LF>
                 ------------------------------------------------------------------------------
-                
-                Field Number: 
-                
+
+                Field Number:
+
                 1. Magnetic Sensor heading in degrees
                 2. Magnetic Deviation, degrees
                 3. Magnetic Deviation direction, E = Easterly, W = Westerly, direction of magnetic north from true north.
                 4. Magnetic Variation degrees
                 5. Magnetic Variation direction, E = Easterly, W = Westerly
                 6. Checksum
-                
+
                 */
                 char hdgSentence [35];
                 float x = (abs(declination));
@@ -449,7 +455,7 @@ void printNmeaMag(float h){
                 //x=((int)(x*10))*0.1;
                 PString hdgStr(hdgSentence, sizeof(hdgSentence));
         	hdgStr.print("$HCHDG,");
-                hdgStr.print(h);//mag heading 
+                hdgStr.print(h);//mag heading
         	hdgStr.print(",");
                 hdgStr.print(xBuf);//declination
                 if(declination<0.0){
@@ -463,9 +469,9 @@ void printNmeaMag(float h){
                 }else{
                   hdgStr.print(",W*");
                 }
-        	
+
         	//calculate the checksum
-        
+
         	cs = 0; //clear any old checksum
         	for (unsigned int n = 1; n < strlen(hdgSentence) - 1; n++) {
         		cs ^= hdgSentence[n]; //calculates the checksum
@@ -473,18 +479,18 @@ void printNmeaMag(float h){
         	//bug - arduino prints 0x007 as 7, 0x02B as 2B, so we add it now
 		if (cs < 0x10) str.print('0');
         	hdgStr.print(cs, HEX); // Assemble the final message and send it out the serial port
-        	Serial.println(hdgSentence);  
-                
+        	Serial.println(hdgSentence);
+
         }
 
-  
+
 }
 /*
 $YXXDR (Transducer Measurements: Vessel Attitude) Used for various secondary transducers?!
 or $PTNTHPR (Heading, Pitch, Roll) Looks like a new specific attitude one from the digital camera world as apparently EXIF tags also contain NMEA strings.
 
 
-$YXXDR,<1>, <2>, <3>, <4>,<5>, <6>, <7>, <8>,<9>, <10>,<11>,<12>,<13>,<14>,<15>,<16>*hh<CR><LF> 
+$YXXDR,<1>, <2>, <3>, <4>,<5>, <6>, <7>, <8>,<9>, <10>,<11>,<12>,<13>,<14>,<15>,<16>*hh<CR><LF>
 eg $YXXDR,A,5.2,D,PTCH,A,7.4,D,ROLL,,,,,,,,*hh<CR><LF>
 
 The fields in the B version of the XDR sentence are as follows:
@@ -499,7 +505,7 @@ The fields in the B version of the XDR sentence are as follows:
 <9>+Not used
 */
 void printPitchRoll(){
-  
+
     char xdrSentence [65];
 
 	PString str(xdrSentence, sizeof(xdrSentence));
@@ -529,14 +535,14 @@ void printPitchRoll(){
 	if (cs < 0x10) str.print('0');
 	str.print(cs, HEX); // Assemble the final message and send it out the serial port
 	Serial.println(xdrSentence);
-        
+
 }
-   
+
 
 void updateROT(){
   //executed every 0.2 secs
      //value is deg/s
-     float rot=(val[5]*0.06097);
+     double rot=(val[5]*0.06097);
      rotList.addValue(-degrees(rot));
 }
 
@@ -547,24 +553,54 @@ void calcMagHeading(){
   float sin_roll;
   float cos_pitch;
   float sin_pitch;
-  
+
   cos_roll = cos(-yprm[2]);
   sin_roll = sin(-yprm[2]);
   cos_pitch = cos(yprm[1]);
   sin_pitch = sin(yprm[1]);
-  
+
   //Example calc
   //Xh = bx * cos(theta) + by * sin(phi) * sin(theta) + bz * cos(phi) * sin(theta)
   //Yh = by * cos(phi) - bz * sin(phi)
   //return wrap((atan2(-Yh, Xh) + variation))
-    
+
   // Tilt compensated Magnetic field X component:
   Head_X = val[6]*cos_pitch+val[7]*sin_roll*sin_pitch+val[8]*cos_roll*sin_pitch;
   // Tilt compensated Magnetic field Y component:
   Head_Y = val[7]*cos_roll-val[8]*sin_roll;
   // Magnetic Heading
   yprm[3] = atan2(-Head_Y,-Head_X);
-  
+
+}
+
+void SK_output(float h){
+    //heading
+    Serial.print("{\"context\": \"vessels.self\",\"updates\": [{\"values\": [");
+    Serial.print("{\"path\": \"navigation.headingMagnetic\",\"value\":");
+    Serial.print(h*PI/180.0,8);
+    Serial.print("},");
+    //rot
+    Serial.print("{\"path\": \"navigation.rateOfTurn\",\"value\":");
+    Serial.print(rotList.getTotalAverage()*PI/(180.0*60.0),8);//rad/s
+    Serial.print("},");
+    //attitude
+    Serial.print("{\"path\": \"navigation.attitude\",\"value\": {");
+    Serial.print("\"roll\":");
+    Serial.print(degrees(yprm[2])*PI/180.0,8);//rad
+    Serial.print(",\"pitch\":");
+    Serial.print(degrees(yprm[1])*PI/180.0,8);//rad
+    Serial.print(",\"yaw\":");
+    Serial.print(degrees(yprm[0])*PI/180.0,8);//rad
+    Serial.print("}}");
+    //temp
+    /*Serial.print("{\"path\": \"environment.inside.temperature\",\"value\":");
+    Serial.print(273.15f + temperature, 2);
+    Serial.print("},");
+    //pressure
+    Serial.print("{\"path\": \"environment.outside.pressure\",\"value\":");
+    Serial.print(pressure * 100);
+    Serial.print("}");*/
+    Serial.println("],\"source\":{\"label\":\"pesky.IMU\"}}]}");
 }
 
 char serial_busy_wait() {
